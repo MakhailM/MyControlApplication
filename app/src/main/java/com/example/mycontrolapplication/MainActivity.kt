23932b63ba5c55b7fe5098a526af48edb8e351fc
user <user@user.ru>
2022-11-24 16:53:57 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mycontrolapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mycontrolapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+            }
+    fun showActivitySecond(view: View){
+      val SecondActivity = Intent(this,SecondActivity::class.java)
+      startActivity(SecondActivity)
+    }
+
+    }
+
+
+
+
+
